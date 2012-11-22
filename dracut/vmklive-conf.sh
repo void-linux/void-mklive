5159224c3da5c7b2faa06c4b1ae6aeeed62b2e29
Juan RP <xtraeme@gmail.com>
2012-11-22 12:19:12 +0100

    dracut: make sure getarg function is defined.

--- dracut/vmklive-conf.sh
+++ dracut/vmklive-conf.sh
@@ -2,6 +2,8 @@
 # -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
+type getarg >/dev/null 2>&1 || . /lib/dracut-lib.sh
+
 KEYMAP=$(getarg vconsole.keymap)
 FONT=$(getarg vconsole.font)
 FONT_MAP=$(getarg vconsole.font.map)
