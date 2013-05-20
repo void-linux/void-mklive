997b3ca895583541a3aa40a95d9e188419279c8f
Juan RP <xtraeme@gmail.com>
2013-05-20 13:15:21 +0200

    dracut: misc tweaks.

--- dracut/adduser.sh
+++ dracut/adduser.sh
@@ -12,18 +12,17 @@ echo "USERNAME=$USERNAME" >> ${NEWROOT}/etc/default/live.conf
 chmod 644 ${NEWROOT}/etc/default/live.conf
 
 # Create new user and remove password. We'll use autologin by default.
-chroot ${NEWROOT} useradd -c $USERNAME -m $USERNAME -G systemd-journal,wheel -s /bin/bash
-chroot ${NEWROOT} passwd -d $USERNAME 2>&1 >/dev/null
+chroot ${NEWROOT} useradd -c $USERNAME -m $USERNAME -G \
+    systemd-journal,wheel -s /bin/bash
+chroot ${NEWROOT} passwd -d $USERNAME >/dev/null 2>&1
 
 # Enable sudo permission by default.
 if [ -f ${NEWROOT}/etc/sudoers ]; then
-	echo "${USERNAME}  ALL=(ALL) NOPASSWD: ALL" >> ${NEWROOT}/etc/sudoers
+    echo "${USERNAME}  ALL=(ALL) NOPASSWD: ALL" >> ${NEWROOT}/etc/sudoers
 fi
 
 # Enable autologin for agetty(8).
-if [ -f ${NEWROOT}/usr/lib/systemd/system/getty@.service ]; then
-        rm -f "${NEWROOT}/etc/systemd/system/getty.target.wants/getty@tty1.service"
-	sed -e "s|/sbin/agetty --noclear|& -a ${USERNAME}|g" \
-                "${NEWROOT}/usr/lib/systemd/system/getty@.service" > \
-                "${NEWROOT}/etc/systemd/system/getty.target.wants/getty@tty1.service"
-fi
+rm -f "${NEWROOT}/etc/systemd/system/getty.target.wants/getty@tty1.service"
+sed -e "s|/sbin/agetty --noclear|& -a ${USERNAME}|g" \
+    "${NEWROOT}/usr/lib/systemd/system/getty@.service" > \
+    "${NEWROOT}/etc/systemd/system/getty.target.wants/getty@tty1.service"
