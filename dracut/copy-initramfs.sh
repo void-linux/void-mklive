0c3ec890bc633206443ad0569a2900c44c98797b
Juan RP <xtraeme@gmail.com>
2013-05-22 08:08:02 +0200

    dracut/copy-initramfs.sh: set $KVER to make this work again.

--- dracut/copy-initramfs.sh
+++ dracut/copy-initramfs.sh
@@ -3,4 +3,5 @@
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
 # Copy the initramfs back to the new rootfs for proper shutdown.
+KVER=$(uname -r)
 cp /run/initramfs/live/boot/initrd.lz $NEWROOT/boot/initramfs-${KVER}.img
