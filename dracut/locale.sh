783a25e632139846c592990413abba8d98cb5164
Juan RP <xtraeme@gmail.com>
2013-08-22 11:52:07 +0200

    Misc changes to reduce image size.
    1- Stop generating locale via dracut; generate choosen locale at build time.
    2- Stop generating a local repository (/packages) to the rootfs.
    3- Drop support for -l option.
    4- There's no need to copy kernel modules manually anymore.
    5- Switch to base-system and some more additional packages to drop base-system-live
       requirement.
    
    WIP! the installer must be updated to copy rootfs files to the targetdir.

--- dracut/locale.sh
+++ dracut/locale.sh
@@ -2,16 +2,10 @@
 # -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
-# Regen locales if it's set in the kernel cmdline.
-
 type getarg >/dev/null 2>&1 || . /lib/dracut-lib.sh
 
 LOCALE=$(getarg locale.LANG)
 [ -z "$LOCALE" ] && LOCALE="en_US.UTF-8"
-
-# Create new user and remove password. We'll use autologin by default.
-sed -e "s,^\#\($LOCALE.*\),\1," -i $NEWROOT/etc/default/libc-locales
-chroot $NEWROOT xbps-reconfigure -f glibc-locales >/dev/null 2>&1
 
 # also enable this locale in newroot.
 echo "LANG=$LOCALE" > $NEWROOT/etc/locale.conf
