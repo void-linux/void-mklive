ab3a398875ca1a6acd701dda74257644f078a4fb
Juan RP <xtraeme@gmail.com>
2014-09-20 11:14:29 +0200

    mklive: set live user shell to /bin/sh; use /etc/issue; disable autologin.

--- dracut/adduser.sh
+++ dracut/adduser.sh
@@ -14,7 +14,7 @@ echo "USERNAME=$USERNAME" >> ${NEWROOT}/etc/default/live.conf
 chmod 644 ${NEWROOT}/etc/default/live.conf
 
 # Create new user and remove password. We'll use autologin by default.
-chroot ${NEWROOT} useradd -c $USERNAME -m $USERNAME -G wheel -s /bin/bash
+chroot ${NEWROOT} useradd -c $USERNAME -m $USERNAME -G wheel -s /bin/sh
 chroot ${NEWROOT} passwd -d $USERNAME >/dev/null 2>&1
 
 # Setup default root password (voidlinux).
@@ -23,21 +23,6 @@ chroot ${NEWROOT} sh -c 'echo "root:voidlinux" | chpasswd -c SHA512'
 # Enable sudo permission by default.
 if [ -f ${NEWROOT}/etc/sudoers ]; then
     echo "${USERNAME}  ALL=(ALL) NOPASSWD: ALL" >> ${NEWROOT}/etc/sudoers
-fi
-
-# Enable autologin for agetty(8) on tty1 with runit.
-if [ -d ${NEWROOT}/etc/runit ]; then
-    sed "s|agetty|& -a $USERNAME|" -i ${NEWROOT}/etc/sv/agetty-tty1/run
-fi
-
-# Enable autologin for agetty(8) on tty1 with systemd.
-if [ -d ${NEWROOT}/etc/systemd/system ]; then
-    rm -f "${NEWROOT}/etc/systemd/system/getty.target.wants/getty@tty1.service"
-    sed -e "s|/sbin/agetty --noclear|& -a ${USERNAME}|g" \
-        "${NEWROOT}/usr/lib/systemd/system/getty@.service" > \
-        "${NEWROOT}/etc/systemd/system/getty@.service"
-    ln -sf /etc/systemd/system/getty@.service \
-        "${NEWROOT}/etc/systemd/system/getty.target.wants/getty@tty1.service"
 fi
 
 if [ -d ${NEWROOT}/etc/polkit-1 ]; then
