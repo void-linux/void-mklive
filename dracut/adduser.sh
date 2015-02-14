ca8d1ff70bbd4ffbb3538c919e17642927f571b4
Juan RP <xtraeme@gmail.com>
2015-02-14 12:44:10 +0100

    dracut/adduser: add user to the audio/video groups.

--- dracut/adduser.sh
+++ dracut/adduser.sh
@@ -23,7 +23,7 @@ if ! grep -q ${USERSHELL} ${NEWROOT}/etc/shells ; then
 fi
 
 # Create new user and remove password. We'll use autologin by default.
-chroot ${NEWROOT} useradd -m -c $USERNAME -G wheel -s $USERSHELL $USERNAME
+chroot ${NEWROOT} useradd -m -c $USERNAME -G audio,video,wheel -s $USERSHELL $USERNAME
 chroot ${NEWROOT} passwd -d $USERNAME >/dev/null 2>&1
 
 # Setup default root/user password (voidlinux).
