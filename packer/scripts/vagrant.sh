5859e79c292b832cf8f0389c75007343fbcb9b23
Michael Aldridge <aldridge.mac@gmail.com>
2019-02-26 21:51:23 -0800

    packer: Initial packer config

---
+++ packer/scripts/vagrant.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Set up sudo
+echo '%vagrant ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/vagrant
+echo 'Defaults:vagrant !requiretty' >> /etc/sudoers.d/vagrant
+chmod 0440 /etc/sudoers.d/vagrant
+
+gpasswd -d vagrant wheel
+
+sudo xbps-install -Sy wget
+
+# Installing vagrant keys
+mkdir /home/vagrant/.ssh
+chmod 700 /home/vagrant/.ssh
+cd /home/vagrant/.ssh
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
+chmod 600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant /home/vagrant/.ssh
+
+# Install NFS for Vagrant
+xbps-install -Sy nfs-utils
