e7f28f260ed499375850f61ef6a1a4ea01138006
classabbyamp <void@placeviolette.net>
2024-12-21 13:21:13 -0500

    build-x86-images: add tmux
    it's only a MB, meh
    
    fixes: #227

--- build-x86-images.sh
+++ build-x86-images.sh
@@ -74,7 +74,7 @@ build_variant() {
     IMG=void-live-${ARCH}-${DATE}-${variant}.iso
     GRUB_PKGS="grub-i386-efi grub-x86_64-efi"
     A11Y_PKGS="espeakup void-live-audio brltty"
-    PKGS="dialog cryptsetup lvm2 mdadm void-docs-browse xtools-minimal xmirror chrony $A11Y_PKGS $GRUB_PKGS"
+    PKGS="dialog cryptsetup lvm2 mdadm void-docs-browse xtools-minimal xmirror chrony tmux $A11Y_PKGS $GRUB_PKGS"
     XORG_PKGS="xorg-minimal xorg-input-drivers xorg-video-drivers setxkbmap xauth font-misc-misc terminus-font dejavu-fonts-ttf orca"
     SERVICES="sshd chronyd"
 
