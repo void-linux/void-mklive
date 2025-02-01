97416b732b5e3277580e21c8c2aa1bcc501ee527
classabbyamp <void@placeviolette.net>
2025-02-01 18:37:58 -0500

    dracut/vmklive/nomodeset: create down in /etc/sv instead
    in the initramfs, the symlinks in /etc/runit/runsvdir/default are
    broken, so it fails to disable the services

--- dracut/vmklive/nomodeset.sh
+++ dracut/vmklive/nomodeset.sh
@@ -6,8 +6,8 @@ type getargbool >/dev/null 2>&1 || . /lib/dracut-lib.sh
 
 if getargbool 0 nomodeset; then
     for dm in lightdm sddm gdm; do
-        if [ -e "${NEWROOT}/etc/runit/runsvdir/default/${dm}" ]; then
-            touch "${NEWROOT}/etc/runit/runsvdir/default/${dm}/down"
+        if [ -e "${NEWROOT}/etc/sv/${dm}" ]; then
+            :> "${NEWROOT}/etc/sv/${dm}/down"
         fi
     done
 fi
